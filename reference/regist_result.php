e8f60db8c624e405cd52a8a17bb80045e312dbdc
KenMitsu <km3zch1058@gmail.com>
2021-03-04 17:59:31 +0900

    commit

--- reference/regist_result.php
+++ reference/regist_result.php
@@ -1,6 +1,6 @@
 <?php
-//require_once('../config.php');
-//reqire_once('../class.php');
+require_once('../config.php');
+require_once('../class.php');
 ?>
 
 <!DOCTYPE html>
@@ -37,7 +37,7 @@
             <fieldset class='text-center'>  <!--FIELDSETはフォームの入力項目をグループ化する-->
               <legend>Account Registration</legend>　<!--<LEGEND>～</LEGEND>で入力項目グループにタイトルをつける-->
               <div class='form-group'>
-                  <?php /*
+                  <?php 
                       //データベースへ接続
                       //$pdo = new Twitter;
                       //$pdo->getDBH();
@@ -79,7 +79,7 @@
                         <br>
                         <button type="submit"class="btn btn-default" onclick="location.href='./regist.php'">Back to Account Registration Page</button>
                       EOH;
-                      } */
+                      } 
                       ?>
               </div>
             </fieldset>
