9419639018ae2e203d3b2ebe25608d6e2d8d62b8
KenMitsu <km3zch1058@gmail.com>
2021-03-24 17:28:10 +0900

    commit

--- reference/tables.php
+++ reference/tables.php
@@ -81,24 +81,6 @@ session_start();
         <ul class='breadcrumb' id='breadcrumb'>
           <li class='title'>Tables</li>
         </ul>
-
-        <div id='toolbar'>
-          <div class='btn-group'>
-            <a class='btn' data-toggle='toolbar-tooltip' href='#' title='Building'>
-              <i class='icon-building'></i>
-            </a>
-            <a class='btn' data-toggle='toolbar-tooltip' href='#' title='Laptop'>
-              <i class='icon-laptop'></i>
-            </a>
-            <a class='btn' data-toggle='toolbar-tooltip' href='#' title='Calendar'>
-              <i class='icon-calendar'></i>
-              <span class='badge'>3</span>
-            </a>
-            <a class='btn' data-toggle='toolbar-tooltip' href='#' title='Lemon'>
-              <i class='icon-lemon'></i>
-            </a>
-          </div>
-        </div>
       </section>
 
 
