24f94b8bb4b06406b514022f24ce349e029c8d9d
KenMitsu <km3zch1058@gmail.com>
2021-03-24 18:16:38 +0900

    commit

--- reference/tables_like.php
+++ reference/tables_like.php
@@ -23,8 +23,8 @@ session_start();
     <link href="assets/stylesheets/application-a07755f5.css" rel="stylesheet" type="text/css" /><link href="//netdna.bootstrapcdn.com/font-awesome/3.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/images/favicon.ico" rel="icon" type="image/ico" />
     <style>
-      td { 
-      background-color: #4682B4;
+      table td:first-child{
+      background-color:red;
       }
     </style>
   </head>
