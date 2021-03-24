0664e42e72886f248d6341d96e9034151d062170
KenMitsu <km3zch1058@gmail.com>
2021-03-24 17:00:13 +0900

    commit

--- reference/tables.php
+++ reference/tables.php
@@ -41,7 +41,7 @@ session_start();
         <li class='dropdown user'>
           <a class='dropdown-toggle' data-toggle='dropdown' href='#'>
             <i class='icon-user'></i>
-            <strong>$row['nickname'];</strong>
+            <strong>$row['nickname']</strong>
             <img class="img-rounded" src="http://placehold.it/20x20/ccc/777" />
             <b class='caret'></b>
           </a>
