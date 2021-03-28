6761a233993804488d5ef2617b219d41ab0dda2b
KenMitsu <km3zch1058@gmail.com>
2021-03-28 11:50:29 +0900

    commit

--- getTweet.php
+++ getTweet.php
@@ -3,8 +3,7 @@ require_once(__DIR__ . '/class.php');
 
 $tweet = new Tweet;
 
-$tweet->getTweet(10000);
-
+$tweet->getTweet(10);
 $tweet->redaction('user_info', 'user_id');
 $tweet->redaction('tweet_info', 'contents');
 ?>
